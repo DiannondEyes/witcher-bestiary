53468192a2be6f308b506c85354aa8d3137b5a97
MALWARE <notmalware@inbox.ru>
2023-04-01 10:08:47 +0300

    Initial commit

---
+++ app/src/main/java/com/mw/bestiarii/MainActivity.java
@@ -0,0 +1,14 @@
+package com.mw.bestiarii;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
